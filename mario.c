4b3dba0ee79a8acd6ce6874535fea2e65f898f30
alimak1 <69662507+alimak1@users.noreply.github.com>
2020-08-13 23:13:56 -0500

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  int n;
+  do
+  {
+    n = get_int("height: ");
+  }
+  while (n < 1 || n > 8);
+
+    for (int i = 0; i < n; i++)
+     {
+        for (int j = n - 1; j > i; j--)
+        {
+          printf(" ");
+        }  
+        for (int k = 0; k <= i; k++)
+        {
+          printf("#");
+        }
+        for(int l = i; l  == i; l++)
+        {
+          printf("  ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+          printf("#");
+        }
+        printf("\n");
+    }
+}
+
